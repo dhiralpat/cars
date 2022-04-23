69decc22773af32ecad2faffb097d095b3e1130a
Ali <ali@itexps>
2022-04-23 01:01:42 -0500

    SACC-11 initial project commit and search func test

---
+++ src/test/java/com/cars/automationtest/Cars.java
@@ -0,0 +1,92 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
+ */
+package com.cars.automationtest;
+
+import java.util.concurrent.TimeUnit;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
+
+/**
+ *
+ * @author Owner
+ */
+public class Cars {
+
+    private WebDriver driver;
+    private String baseUrl;
+
+    public Cars() {
+    }
+
+    @BeforeAll
+    public static void setUpClass() {
+    }
+
+    @AfterAll
+    public static void tearDownClass() {
+    }
+
+    @BeforeEach
+    public void setUp() {
+        System.setProperty("webdriver.gecko.driver", "c:\\data\\geckodriver.exe");
+        driver = new FirefoxDriver();
+        baseUrl = "https://www.cars.com/";
+        driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
+    }
+
+    @AfterEach
+    public void tearDown() {
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    @Test
+    public void carSearchTest() throws Exception  {
+        driver.get("https://www.cars.com/");
+        Select sc = new Select(driver.findElement(By.name("stock_type")));
+        sc.selectByIndex(3);
+        Thread.sleep(2000);
+        
+        Select sc2 = new Select(driver.findElement(By.id("makes")));
+        sc2.selectByVisibleText("Lexus");
+        Thread.sleep(2000);
+        
+        Select sc3 = new Select(driver.findElement(By.id("models")));
+        sc3.selectByIndex(41);
+        Thread.sleep(2000);
+        
+        Select sc4 = new Select(driver.findElement(By.name("list_price_max")));
+        sc4.selectByIndex(16);
+        Thread.sleep(2000);
+        
+        Select sc5 = new Select(driver.findElement(By.id("make-model-maximum-distance")));
+        sc5.selectByVisibleText("50 miles");
+        Thread.sleep(2000);
+        
+        driver.findElement(By.id("make-model-zip")).clear();
+        driver.findElement(By.id("make-model-zip")).sendKeys("60169");
+        Thread.sleep(2000);
+        
+        driver.findElement(By.xpath("//*[@id=\"by-make-tab\"]/div/div[7]/button")).click();
+        Thread.sleep(2000);
+        
+        
+       // driver.findElement(By.id("881e9798-3ee2-4fbd-9ed0-8c28d080007a")).click();
+       // Thread.sleep(10000);
+        driver.close();
+        
+        
+
+    }
+}
