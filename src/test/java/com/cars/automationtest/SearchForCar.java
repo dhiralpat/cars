5c49dc7758cf9ad0e5d42f4a0a7596b214131c91
Ali <ali@itexps>
2022-05-02 21:11:30 -0500

    SACC-22 Add Review and research testcase automation

--- src/test/java/com/cars/automationtest/SearchForCar.java
+++ src/test/java/com/cars/automationtest/SearchForCar.java
@@ -12,8 +12,12 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.interactions.Action;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.Select;
 
 /**
@@ -88,5 +92,44 @@ public class SearchForCar {
         
         
 
+    }
+    @Test
+    public void researchAndReviewTest() throws InterruptedException{
+        driver.get(baseUrl);
+        driver.manage().window().maximize();
+       
+        driver.findElement(By.xpath("//*[@id=\"mobile-menu-section\"]/ul[1]/li[2]/a")).click();
+         JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("window.scrollTo(0, 600)");
+        Thread.sleep(2000);
+        
+         //driver.findElement(By.id("make-select")).click();
+        Select sc = new Select(driver.findElement(By.id("make-select")));
+        sc.selectByIndex(38);
+        Thread.sleep(2000);
+       
+        Select sc1 = new Select(driver.findElement(By.name("model")));
+        sc1.selectByVisibleText("LX 570");
+        Thread.sleep(2000);
+        
+        JavascriptExecutor js1 = (JavascriptExecutor) driver;
+        js.executeScript("window.scrollTo(0, 600)");
+        Select sc2 = new Select(driver.findElement(By.name("year")));
+        sc2.selectByIndex(3);
+        Thread.sleep(3000);
+        
+        driver.findElement(By.xpath("//*[@id=\"by-search-tab\"]/div/div/div[4]/button")).click();
+        JavascriptExecutor js2 = (JavascriptExecutor) driver;
+        js.executeScript("window.scrollTo(0, 600)");
+        Thread.sleep(2000);
+        
+        driver.findElement(By.xpath("//*[@id=\"ae-main-content\"]/div[2]/div[2]/a")).click();
+        
+        
+        
+        
+       
+        
+        
     }
     }
