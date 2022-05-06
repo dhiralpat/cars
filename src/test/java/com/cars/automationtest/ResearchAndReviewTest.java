e6e15b697a66322593e716bdf041d9d4c7065c0a
Ali <ali@itexps>
2022-05-05 20:33:56 -0500

    SACC-22 add carpart test

--- src/test/java/com/cars/automationtest/ResearchAndReviewTest.java
+++ src/test/java/com/cars/automationtest/ResearchAndReviewTest.java
@@ -1,8 +1,10 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package com.cars.automationtest;
+
+
 
 import java.util.concurrent.TimeUnit;
 import org.junit.jupiter.api.AfterEach;
@@ -24,12 +26,12 @@ import org.openqa.selenium.support.ui.Select;
  *
  * @author Owner
  */
-public class SearchForCar {
+public class ResearchAndReviewTest {
 
     private WebDriver driver;
     private String baseUrl;
 
-    public SearchForCar() {
+    public ResearchAndReviewTest() {
     }
 
     @BeforeAll
@@ -49,49 +51,47 @@ public class SearchForCar {
     public void tearDown() {
     }
 
-    // TODO add test methods here.
-    // The methods must be annotated with annotation @Test. For example:
-    //
     @Test
-    public void carSearchTest() throws Exception  {
+    public void researchAndReviewTest() throws InterruptedException{
         System.setProperty("webdriver.chrome.driver", "C:\\data\\chromedriver.exe");
         driver = new ChromeDriver();
         baseUrl = "https://www.cars.com/";
         driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
         driver.get(baseUrl);
-        Select sc = new Select(driver.findElement(By.name("stock_type")));
-        sc.selectByIndex(3);
+        driver.manage().window().maximize();
+       
+        driver.findElement(By.xpath("//*[@id=\"mobile-menu-section\"]/ul[1]/li[2]/a")).click();
+         JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("window.scrollTo(0, 600)");
         Thread.sleep(2000);
         
-        Select sc2 = new Select(driver.findElement(By.id("makes")));
-        sc2.selectByVisibleText("Lexus");
+         //driver.findElement(By.id("make-select")).click();
+        Select sc = new Select(driver.findElement(By.id("make-select")));
+        sc.selectByIndex(38);
+        Thread.sleep(2000);
+       
+        Select sc1 = new Select(driver.findElement(By.name("model")));
+        sc1.selectByVisibleText("LX 570");
         Thread.sleep(2000);
         
-        Select sc3 = new Select(driver.findElement(By.id("models")));
-        sc3.selectByIndex(41);
-        Thread.sleep(2000);
-        
-        Select sc4 = new Select(driver.findElement(By.name("list_price_max")));
-        sc4.selectByIndex(16);
-        Thread.sleep(2000);
-        
-        Select sc5 = new Select(driver.findElement(By.id("make-model-maximum-distance")));
-        sc5.selectByVisibleText("50 miles");
-        Thread.sleep(2000);
-        
-        driver.findElement(By.id("make-model-zip")).clear();
-        driver.findElement(By.id("make-model-zip")).sendKeys("60169");
-        Thread.sleep(2000);
-        
-        driver.findElement(By.xpath("//*[@id=\"by-make-tab\"]/div/div[7]/button")).click();
+        JavascriptExecutor js1 = (JavascriptExecutor) driver;
+        js.executeScript("window.scrollTo(0, 600)");
+        Select sc2 = new Select(driver.findElement(By.name("year")));
+        sc2.selectByIndex(3);
         Thread.sleep(3000);
         
+        driver.findElement(By.xpath("//*[@id=\"by-search-tab\"]/div/div/div[4]/button")).click();
+        JavascriptExecutor js2 = (JavascriptExecutor) driver;
+        js.executeScript("window.scrollTo(0, 600)");
+        Thread.sleep(2000);
         
-       // driver.findElement(By.id("881e9798-3ee2-4fbd-9ed0-8c28d080007a")).click();
-       // Thread.sleep(10000);
-        driver.close();
+        driver.findElement(By.xpath("//*[@id=\"ae-main-content\"]/div[2]/div[2]/a")).click();
         
         
-
+        
+        
+       
+        
+        
     }
     }
